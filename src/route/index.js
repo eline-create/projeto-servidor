2de60e0c78a6f942c741b2cb706ed8d1d27107f2
eline-create <eline.geo@gmail.com>
2020-11-10 22:06:26 -0300

    Atualizando o index.js

--- src/route/index.js
+++ src/route/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.status(200).send({
         title: "As Linguagens de Programação mais populares da atualidade",
-        data: "21/09/2020",
+        date: "21/09/2020 - version 1.0.0",
+        currentDate: "10/11/2020",
         version: "2.0.0"
     })
 });
